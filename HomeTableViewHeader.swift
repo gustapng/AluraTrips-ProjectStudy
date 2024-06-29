ccd190568b281a8975cac44242a1409577460fdb
Gustavo Ferreira <guuxts@gmail.com>
2024-06-29 11:14:49 -0300

    updating files

--- HomeTableViewHeader.swift
+++ HomeTableViewHeader.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class HomeTableViewHeader: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: -IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var bannerImageView: UIImageView!
+    
+    func configView() {
+        headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
     }
-    */
-
 }
