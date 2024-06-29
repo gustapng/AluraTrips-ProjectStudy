d81b4afed685a15d3b8b49bfc7367aeb3467b29c
Gustavo Ferreira <guuxts@gmail.com>
2024-06-29 11:13:34 -0300

    updating files

---
+++ HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  AluraTrips
+//
+//  Created by Gustavo Ferreira dos Santos on 29/06/24.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
